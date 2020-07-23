8dff6b138802f82431652d91827615d8b4c74e3e
Patel Krushankant <patelkk1618@gmail.com>
2020-07-23 18:37:19 -0500

    getEmployees data to display.

--- src/app/Employee/employee-list/employee-list.component.ts
+++ src/app/Employee/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {EmployeeService} from "../../shared/employee.service";
 
 @Component({
   selector: 'employee-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _empService: EmployeeService) { }
 
   ngOnInit(): void {
+    this._empService.getEmployees().subscribe(
+      list => {
+        let array = list.map(item => {
+          return {
+            $key: item.key,
+            ...item.payload.val()
+          };
+        });
+      });
   }
-
 }
