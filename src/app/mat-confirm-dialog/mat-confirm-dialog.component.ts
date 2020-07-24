d12ba1fb9f6aa9c528d8732f41353d7c5cbacd77
Patel Krushankant <patelkk1618@gmail.com>
2020-07-24 13:34:46 -0500

    closeDialog() on Click

--- src/app/mat-confirm-dialog/mat-confirm-dialog.component.ts
+++ src/app/mat-confirm-dialog/mat-confirm-dialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 //To received data here we need to import MAT_DIALOG_DATA.
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-mat-confirm-dialog',
@@ -11,8 +11,12 @@ export class MatConfirmDialogComponent implements OnInit {
 
   //need to inject MAT_DIALOG_DATA here too.
   constructor(@Inject (MAT_DIALOG_DATA) public data) { }
-
+  //need to give reference to the dialogComponent.
+  public dialogRef:MatDialogRef<MatConfirmDialogComponent>
   ngOnInit(): void {
   }
 
+  closeDialog() {
+    this.dialogRef.close()
+  }
 }
