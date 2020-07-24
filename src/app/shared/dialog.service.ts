862873406fb7665941b68121756ef5c16961086f
Patel Krushankant <patelkk1618@gmail.com>
2020-07-24 13:16:26 -0500

    Designed the delete popup dialog.

--- src/app/shared/dialog.service.ts
+++ src/app/shared/dialog.service.ts
@@ -12,6 +12,7 @@ export class DialogService {
   openConfirmDialog(){
     this._dialog.open(MatConfirmDialogComponent,{
       width: '390px',
+      panelClass: 'confirm-dialog-container',
       disableClose:true
     });
   }
